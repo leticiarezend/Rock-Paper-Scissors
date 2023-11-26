813a16ebe0600c5e2b6b020b3473cc8eea5fae73
Leticia Rezende <leticia.rezende1020@outlook.com>
2023-11-26 20:31:07 -0300

    Adiciona função playRound

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 
+const opcoes = ['Rock', 'Paper', 'Scissors'];
+
 function getComputerChoice(listaOpcoes) {
     const indice = Math.floor(Math.random() * 3);
     return listaOpcoes[indice];
 }
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    var youLose = (playerSelection == "rock" && computerSelection == "paper") 
+                  || (playerSelection == "paper" && computerSelection == "scissors") 
+                  || (playerSelection == "scissors" && computerSelection == "rock");
+    if(youLose){
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+    else if (playerSelection == computerSelection){
+        return `It's a draw! Your choice was ${playerSelection} and the computer's choice was also  ${computerSelection}`
+    }
+    else{
+        return `You Won! ${playerSelection} beats ${computerSelection} `
+    }
 
-const opcoes = ['Rock', 'Paper', 'Scissors'];
-console.log(getComputerChoice(opcoes));
+}
+
+let compChoice =  getComputerChoice(opcoes);
+let playerChoice = "scissors";
+
+
+console.log("Computer's choice: " + compChoice);
+console.log("Your choice: " + playerChoice);
+console.log("_______ Game_____")
+console.log(playRound(playerChoice, compChoice));
