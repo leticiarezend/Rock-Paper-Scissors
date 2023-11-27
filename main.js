5e84efc64d38efc68d8e5d76a559443498506cc6
Leticia Rezende <leticia.rezende1020@outlook.com>
2023-11-27 14:37:26 -0300

    Adiciona captura e resposta da entrada do usuario

--- main.js
+++ main.js
@@ -58,3 +58,73 @@ function countRounds() {
 //     }
 // }
 // game()
+
+let numberOfRounds = 0;
+let playerPoints = 0;
+let computerPoints = 0;
+
+
+
+let submissionCount = 0;
+
+document.querySelector("form").onsubmit = () => {
+    countRounds();
+    if (submissionCount < 5) {
+        const playerSelection = document.querySelector("#choice").value.toLowerCase();
+
+        if (playerSelection != "paper" && playerSelection != "rock" && playerSelection != "scissors") {
+
+            alert("Opção invalida")
+            const keepPlaceholder = document.querySelector("#choice").value;
+            keepPlaceholder.style.color = gray;
+            keepPlaceholder.value = '';
+        }
+        else {
+            const computerSelection = getComputerChoice(opcoes);
+            const result = playRound(playerSelection, computerSelection);
+            const youLose = playerLose(playerSelection, computerSelection);
+
+
+            if (youLose == "draw") {
+
+            } else if (youLose) {
+                computerPoints += 1;
+            }
+
+            else {
+                playerPoints += 1;
+            }
+
+
+            document.querySelector('#scoreboard_text').innerHTML = `You: ${playerPoints} X Computer: ${computerPoints}`;
+            let score = document.getElementById('scoreboard_text');
+            score.classList.add('use_border');
+            document.querySelector('#result_text').innerHTML = result;
+            document.querySelector('#selection').innerHTML = `Computer's choice: ${computerSelection}  <br> x <br> Your choice: ${playerSelection}`;
+
+
+            submissionCount++;
+
+            document.querySelector("#choice").value = '';
+        }
+
+    }
+
+    if (submissionCount >= 5) {
+        document.querySelector('.submit_button').disabled = true;
+
+        let winner;
+
+        if (playerPoints > computerPoints) {
+            winner = "Congratulations!! You are the winner!"
+        } else if (playerPoints == computerPoints) {
+            winner = "Oops, there's no winner. It's a draw!!"
+        }
+        else {
+            winner = "You lose! The computer is the winner";
+        }
+        document.querySelector('#result_text').innerHTML = "The rounds are over! <br>" + winner;
+
+    }
+    return false;
+}
